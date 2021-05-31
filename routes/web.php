a89645eb582635dfe5f8dcd171dd2e611536085f
Truonghunganh250590 <hunganh250590@gmail.com>
2021-05-31 21:42:38 +0700

    hahahha

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
+
+$app->get('/clear-cache', function () {
+    $code = Artisan::call('cache:clear');
+    return 'cache cleared';
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -12,11 +18,16 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+// Route::get('/clear-cache', function () {
+//     $exitCode = Artisan::call('cache:clear');
+//     // return what you want
+// });
 
-Route::get('/clear-cache', function () {
-    Artisan::call('cache:clear');
-    return "Cache is cleared";
-});
+
+// Route::get('/clear-cache', function () {
+//     Artisan::call('cache:clear');
+//     return "Cache is cleared";
+// });
 
 Route::get('/welcome', function () {
     return view('welcome');
