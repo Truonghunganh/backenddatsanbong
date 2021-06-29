48687f199f60478e4125fe507aa8933991382c5d
unknown <hunganh250590@gmail.com>
2021-06-29 17:05:10 +0700

    heroku hoàn thành 4

--- database/migrations/2020_12_24_105244_create_dat_sans_table.php
+++ database/migrations/2020_12_24_105244_create_dat_sans_table.php
@@ -3,7 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+// $table->foreign('idsan')->references('id')->on('sans')->onUpdate('cascade')->onDelete('cascade');
+            
 class CreateDatSansTable extends Migration
 {
     public function up()
@@ -11,9 +12,9 @@ class CreateDatSansTable extends Migration
         Schema::create('datsans', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->bigInteger('idsan')->unsigned();
-            $table->foreign('idsan')->references('id')->on('sans')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('idsan')->references('id')->on('sans');
             $table->bigInteger('iduser')->unsigned();
-            $table->foreign('iduser')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('iduser')->references('id')->on('users');
             $table->dateTime('start_time');
             $table->bigInteger('price');
             $table->boolean('xacnhan');
